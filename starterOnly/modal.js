0ceea6ff3abb394d1a45ac3839ccdb1578a839eb
LazezBZH <lazez.bzh@caramail.fr>
2021-02-27 17:52:03 +0100

    V2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("my-topnav"); //changement de myTopnav (id html) en my-topnav pour avoir un même style de notation des id
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -10,7 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".form-data"); //changement de tous les formData (classe html) en form-data pour avoir un même style de notation des classes
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +20,12 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//consigne 1 fermer le formulaire au clic sur X (inverse de la fonction déjà créée pour l'ouverture)
 
+const fermetureClicX = document.getElementsByClassName("close");
+
+function FermerFormulaire() {
+  modalbg.style.display = "none"; //enlève formulaire
+}
+
+fermetureClicX[0].addEventListener("click", FermerFormulaire); //élément ciblé=premier élément de class close, action au clic
